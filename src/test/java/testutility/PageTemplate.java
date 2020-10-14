22a6c220db881ed448265602ae6901910e7ccde2
Thejas <ninjamaniac87@gmail.com>
2020-10-14 21:43:33 +0200

    Added method to perform click operation and refresh operation

--- src/test/java/testutility/PageTemplate.java
+++ src/test/java/testutility/PageTemplate.java
@@ -2,9 +2,16 @@ package testutility;
 
 import net.serenitybdd.core.pages.PageObject;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 
+import java.util.concurrent.TimeUnit;
+
 public abstract class PageTemplate extends PageObject {
+
+    private static int EXPLICT_WAIT_TIME = 20;
+    private static int IMPLICIT_WAIT_TIME = 10;
+    private static int maxNoOfRetries = 2;
 
     public WebElement findElementByXpath(String xpath){
         try{
@@ -15,4 +22,26 @@ public abstract class PageTemplate extends PageObject {
         }
     }
 
+    public void refreshDriver(){
+        getDriver().navigate().refresh();
+        waitABit(3000);
+    }
+
+    protected void click(WebElement element){
+        int counter = 0;
+
+        while (maxNoOfRetries > 0 && counter!=maxNoOfRetries){
+            try{
+                element.click();break;
+            }catch(Exception e){
+                System.out.println("Exception.." + e.getMessage());
+                getDriver().manage().timeouts().implicitlyWait(IMPLICIT_WAIT_TIME, TimeUnit.SECONDS);
+                JavascriptExecutor executor = (JavascriptExecutor) getDriver();
+                executor.executeScript("arguments[0].click();", element);
+            }
+            counter++;
+        }
+        if (counter == maxNoOfRetries)
+            element.click();
+    }
 }
